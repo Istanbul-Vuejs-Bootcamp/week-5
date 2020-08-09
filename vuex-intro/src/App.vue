8b63d45f264e18d89e306c4d98f9cdbb2ae9cc31
Mustafa Demir <mustafa.demir@kboxglobal.com>
2020-08-09 10:02:47 +0300

    vuex intro

---
+++ vuex-intro/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      Vuex intro
+        <br>
+        <br>
+        <ShowData />
+        <br>
+        <br>
+        <button @click="increment"> Increment </button>
+        <br>
+        <br>
+        <button @click="increment2"> Increment </button>
+    </div>
+</template>
+
+<script>
+    import ShowData from "./components/ShowData";
+    import { mapActions } from  'vuex';
+    export default {
+        components: { ShowData },
+        methods: {
+            ...mapActions({
+                setCount: 'setCount'
+            }),
+            increment() {
+                this.$store.commit('increment');
+            },
+            increment2() {
+                this.setCount();
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vuex-intro/src/components/ShowData.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="showData">
+        <h3>Data gösterme yöntemleri</h3>
+        Stora direk erişim: {{ $store.state.count }}
+        <hr>
+        Getters ile erişim: {{ $store.getters.getCount }}
+        <hr>
+        mapState ile erişim: {{ count }}
+        <hr>
+        mapGetters ile erişim: {{ myCount }}
+    </div>
+</template>
+
+<script>
+    import { mapState, mapGetters } from 'vuex';
+    export default {
+        name: "ShowData",
+        computed: {
+            ...mapState(['count']),
+            ...mapGetters({
+                myCount: 'getCount'
+            }),
+        },
+        created() {
+        }
+    }
+</script>
+
+<style scoped>
+    .showData {
+        border: solid 1px #ccc;
+        width: 500px;
+        margin: auto;
+        background-color: lavender;
+        padding: 12px;
+    }
+</style>
